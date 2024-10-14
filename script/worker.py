d1a8a4d1d5fd4ca0b4da30c775473f456c6338b2
dsccccc <deshuchen97+dsc@gmail.com>
2024-10-15 00:17:16 +0800

    Update worker.py

--- script/worker.py
+++ script/worker.py
@@ -1,11 +1,9 @@
-import re
-
-def replacer(input_file: str = './tmp/README.md', output_file: str = 'README.md', reg: str = r'/<!-- TABLE_START -->([\s\S]*?)<!-- TABLE_END -->/'):
+def replacer(input_file: str = './tmp/README.md', output_file: str = 'README.md', reg: str = '<!-- TABLE_START --><!-- TABLE_END -->'):
     with open(input_file, 'r') as f:
         data = f.read()
     with open(output_file, 'r') as f:
         table = f.read()
-    data = re.sub(reg, table, data)
+    data = data.replace(reg, table)
     with open(output_file, 'w') as f:
         f.write(table)
 
