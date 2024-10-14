5cf129c419ac43614e08b30ed96e4007fe42bb3c
dsccccc <deshuchen97+dsc@gmail.com>
2024-10-15 00:27:40 +0800

    Update worker.py

--- script/worker.py
+++ script/worker.py
@@ -1,8 +1,6 @@
-def replacer(input_file: str = './tmp/README.md', output_file: str = 'README.md', reg: str = '<!-- TABLE_START --><!-- TABLE_END -->'):
+def replacer(table: str, input_file: str = './tmp/README.md', output_file: str = 'README.md', reg: str = '<!-- TABLE_START --><!-- TABLE_END -->'):
     with open(input_file, 'r') as f:
         data = f.read()
-    with open(output_file, 'r') as f:
-        table = f.read()
     data = data.replace(reg, table)
     with open(output_file, 'w') as f:
         f.write(data)
@@ -11,4 +9,4 @@ if __name__ == '__main__':
     from src.MetroPCS import MetroPCS
     metro = MetroPCS()
     metro.wrapper()
-    replacer()
+    replacer(metro.markdown)
